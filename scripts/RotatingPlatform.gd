e44aff4269203e7b532b8bdd9fbff5bd0b0cd879
3starblaze <3starblaze@protonmail.com>
2021-10-17 19:27:16 +0300

    Add orbit line to rotating platform
    It's meant for the editor but it may be useful for other purposes in the future

--- scripts/RotatingPlatform.gd
+++ scripts/RotatingPlatform.gd
@@ -4,10 +4,13 @@ extends Node2D
 export var radius = 100 setget set_radius
 export var rotational_velocity = 1.07 setget set_rotational_velocity
 onready var collider = $Collider
+onready var orbit_line = $OrbitLine
 
 
 func _ready():
 	collider.position.x = radius
+	if not Engine.editor_hint:
+		orbit_line.visible = false
 
 
 func _physics_process(delta):
@@ -15,11 +18,24 @@ func _physics_process(delta):
 		rotation += rotational_velocity * delta
 
 
+# This is needed for tool because it sometimes runs scripts before it's ready
+func is_ready():
+	return collider != null
+
+
 func tool_refresh():
-	# Sometimes collider is null and in that case do nothing
-	# That probably is caused by tool being executed before collider is loaded
-	if Engine.editor_hint and collider != null:
+	if Engine.editor_hint and is_ready():
 		collider.position.x = radius
+		refresh_orbit()
+
+
+func refresh_orbit():
+	var point_count = 100
+	var angle_delta = 2 * PI / point_count
+	orbit_line.clear_points()
+	for i in range(point_count):
+		orbit_line.add_point(Vector2(radius * cos(i * angle_delta),
+									 radius * sin(i * angle_delta)))
 
 
 func set_radius(value):
