c8fc19c91d3af0d846fd32437815efd3fd0a9d7f
3starblaze <3starblaze@protonmail.com>
2021-10-26 07:54:49 +0300

    Create non-functional ConfirmOverlay

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -2,15 +2,18 @@ extends Control
 
 signal config_button_pressed
 signal collidix_button_pressed
+signal confirm_button_pressed
 const collidix_overlay = preload("res://scenes/CollidixOverlay.tscn")
 onready var hp_label = $Panel/HBoxContainer/HpLabel
 onready var config_button = $Panel/HBoxContainer/ConfigButton
 onready var collidix_button = $Panel/HBoxContainer/CollidixButton
+onready var confirm_button = $Panel/HBoxContainer/ConfirmButton
 
 
 func _ready():
 	collidix_button.connect("pressed", self, "_on_collidix_button_pressed")
 	config_button.connect("pressed", self, "_on_config_button_pressed")
+	confirm_button.connect("pressed", self, "_on_confirm_button_pressed")
 
 
 func _on_config_button_pressed():
@@ -19,6 +22,10 @@ func _on_config_button_pressed():
 
 func _on_collidix_button_pressed():
 	emit_signal("collidix_button_pressed")
+
+
+func _on_confirm_button_pressed():
+	emit_signal("confirm_button_pressed")
 
 
 func set_hp_label(hp: int) -> void:
--- scripts/main.gd
+++ scripts/main.gd
@@ -8,6 +8,7 @@ onready var space_station = $SpaceStation
 onready var meteor_platform_table = $MeteorPlatformTable
 onready var config_overlay = $Overlays/ConfigOverlay
 onready var collidix_overlay = $Overlays/CollidixOverlay
+onready var confirm_overlay = $Overlays/ConfirmOverlay
 
 
 func _ready():
@@ -15,6 +16,7 @@ func _ready():
 	space_station.connect("hp_change", self, "_on_space_station_hp_change")
 	hud.connect("collidix_button_pressed", self, "_on_collidix_button_pressed")
 	hud.connect("config_button_pressed", self, "_on_config_button_pressed")
+	hud.connect("confirm_button_pressed", self, "_on_confirm_button_pressed")
 	# warning-ignore:unused_variable
 	var meteors = [
 		add_meteor(Vector2(0, 40), 50),
@@ -47,6 +49,10 @@ func _on_config_button_pressed() -> void:
 
 func _on_collidix_button_pressed() -> void:
 	collidix_overlay.visible = !collidix_overlay.visible
+
+
+func _on_confirm_button_pressed() -> void:
+	confirm_overlay.visible = !confirm_overlay.visible
 
 
 func add_meteor(pos: Vector2, v: int) -> KinematicBody2D:
