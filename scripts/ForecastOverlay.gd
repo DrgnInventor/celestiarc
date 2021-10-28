247be3bfbbba8f474353d1c3995c33b1305acbb6
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-10-28 15:09:02 +0300

    rename ForecastOverlay inputs

--- scripts/ForecastOverlay.gd
+++ scripts/ForecastOverlay.gd
@@ -6,6 +6,5 @@ onready var forecaster = $Panel/VBoxContainer/Content/Forecaster
 func get_forecast_table():
 	return forecaster
 
-
-func set_table_data(meteor: Array, platform: Array) -> void:
-	forecaster.refresh_data(meteor, platform)
+func set_table_data(mData: Array, pData: Array) -> void:
+	forecaster.refresh_data(mData, pData)
