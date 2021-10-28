3325545f6887b4183326c2619203e62aff4ac051
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-10-28 15:04:13 +0300

    GLOBAL NUMBER ROUNDER

--- scripts/Forecaster.gd
+++ scripts/Forecaster.gd
@@ -7,8 +7,10 @@ func forecast_mformater(data: Array):
 	var mTable = []
 	var format = "Position: %s; Velocity: %s"
 	for meteor in data:
-		var v = meteor.velocity
+		var v = stepify(meteor.velocity, Globals.number_rounder)
 		var pos = CoordUtil.px_to_canon_coord(meteor.global_position)
+		pos.y = stepify(pos.y, Globals.number_rounder)
+		pos.x = stepify(pos.x, Globals.number_rounder)
 		mTable.append(format % [pos, v])
 	return mTable
 
@@ -18,8 +20,8 @@ func forecast_pformater(data: Array):
 	var format = "Original Position: %s; Radius: %s"
 	for platform in data:
 		var pos = CoordUtil.px_to_canon_coord(platform.global_position)
-		pos.y = stepify(pos.y, 1)
-		pos.x = stepify(pos.x, 1)
+		pos.y = stepify(pos.y, Globals.number_rounder)
+		pos.x = stepify(pos.x, Globals.number_rounder)
 		var radius = platform.radius
 		pTable.append(format % [pos, radius])
 	return pTable
