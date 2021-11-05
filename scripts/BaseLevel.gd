8838a5b80424db13910e898de5f90058f642982b
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-11-05 16:47:35 +0200

    Meteor_Collision crash bug [FIX]
    loops through all current $Meteor children and queues them for deletion

--- scripts/BaseLevel.gd
+++ scripts/BaseLevel.gd
@@ -150,7 +150,7 @@ func start_level() -> void:
 func lose_handler() -> void:
 	handle_overlay("lose")
 	Globals.level_running = false
-	for m in current_meteors:
+	for m in $Meteors.get_children():
 		m.queue_free()
 
 
