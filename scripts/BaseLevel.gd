fe983759b551f95f0ea7e2e4a67d6d6cf20135ee
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-11-07 17:27:06 +0200

    precision for lvl4

--- scripts/BaseLevel.gd
+++ scripts/BaseLevel.gd
@@ -42,9 +42,9 @@ func _ready():
 	collidix_overlay.gen_table(current_meteors, current_platforms)
 	forecast_overlay.gen_tables(current_meteors, current_platforms)
 	
-	var m = current_meteors[0] # Meteor whose velocity I want to change
+	var m = current_meteors[1] # Meteor whose velocity I want to change
 	var p = current_platforms[0] # Platform on which meteor will collide
-	var theta = 2.4488 # the rotational_offset I want to get
+	var theta = 4.5044 # the rotational_offset I want to get
 	print("meteor name: ", m.name)
 	print("platform name: ", p.name)
 	p.rotational_offset = theta
